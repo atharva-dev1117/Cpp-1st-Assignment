dcba2cc96c03e5daad7c151827c969824bdc3571
Atharva Jagtap <atharvajagtap171@gmail.com>
2026-03-31 01:01:12 -0400

    Add files via upload

--- ExperimentNo11.cpp
+++ ExperimentNo11.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// ----------------- Compile-Time Polymorphism (Function Overloading) -----------------
-class Calculator
-{
-public:
-// Function to add two integers
-int add(int a, int b)
-{
-return a + b;
-}
-
-// Function to add three integers (overloading)
-int add(int a, int b, int c)
-{
-return a + b + c;
-}
-
-// Function to add two floating-point numbers (overloading)
-float add(float a, float b)
-{
-return a + b;
-}
-};
-
-// ----------------- Run-Time Polymorphism (Function Overriding) -----------------
+// ----------------- Base class for Function Overriding -----------------
 class Shape
 {
 public:
-// Virtual function for area
-virtual void area()
+// Virtual function to display shape type
+
+virtual void display()
 {
-cout << "Area of generic shape" << endl;
+cout << "This is a generic shape." << endl;
 }
 };
 
+// Derived class: Rectangle
 class Rectangle : public Shape
 {
 private:
@@ -43,59 +22,61 @@ float length, width;
 public:
 Rectangle(float l, float w)
 {
-
 length = l;
 width = w;
 }
 
-// Overriding the area function
-void area() override
+// Overriding the display function
+void display() override
 {
-cout << "Area of rectangle = " << length * width << endl;
+cout << "This is a rectangle with length " << length
+<< " and width " << width << "." << endl;
+}
+
+// Function to get area
+float area()
+
+{
+return length * width;
+}
+
+// Operator overloading for + (adding areas of two rectangles)
+float operator+(Rectangle r)
+{
+return this->area() + r.area();
 }
 };
 
-class Circle : public Shape
-{
-private:
-float radius;
-
-public:
-Circle(float r)
-{
-radius = r;
-}
-
-// Overriding the area function
-void area() override
-{
-cout << "Area of circle = " << 3.1416 * radius * radius << endl;
-
-}
-};
-
-// ----------------- Main Function -----------------
+// ----------------- Main function -----------------
 int main()
 {
-// Compile-time polymorphism demonstration
-Calculator calc;
-cout << "--- Compile-Time Polymorphism (Function Overloading) ---\n";
-cout << "Sum of 2 integers: " << calc.add(10, 20) << endl;
-cout << "Sum of 3 integers: " << calc.add(5, 10, 15) << endl;
-cout << "Sum of 2 floats: " << calc.add(2.5f, 3.5f) << endl;
+// Operator Overloading demonstration
+Rectangle rect1(5, 10);
+Rectangle rect2(3, 4);
 
-// Run-time polymorphism demonstration
-Shape *s1, *s2;
-Rectangle rect(10, 5);
-Circle circ(7);
+float totalArea = rect1 + rect2; // Calls overloaded + operator
+cout << "--- Operator Overloading ---" << endl;
+cout << "Area of rect1: " << rect1.area() << endl;
+cout << "Area of rect2: " << rect2.area() << endl;
+cout << "Total area (rect1 + rect2) using overloaded +: " << totalArea << endl;
 
-s1 = &rect; // Base class pointer pointing to derived class object
-s2 = &circ;
+// Function Overriding demonstration
+Shape *s;
+Rectangle rect3(7, 2);
+s = &rect3; // Base class pointer pointing to derived class object
 
-cout << "\n--- Run-Time Polymorphism (Function Overriding) ---\n";
-s1->area(); // Calls Rectangle's area()
-s2->area(); // Calls Circle's area()
+cout << "\n--- Function Overriding ---" << endl;
+s->display(); // Calls Rectangle's display() due to runtime polymorphism
 
 return 0;
-
 }
+
+//OUTPUT
+
+//--- Operator Overloading ---
+//Area of rect1: 50
+//Area of rect2: 12
+//Total area (rect1 + rect2) using overloaded +: 62
+
+//--- Function Overriding ---
+//This is a rectangle with length 7 and width 2.
